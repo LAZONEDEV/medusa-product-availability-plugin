bcb265d57d8e489365131b0935d5b6818d797d61
bocovo <bocovojuste@gmail.com>
2024-01-15 15:24:43 +0100

    feat: create endpoint to get availabilities

--- src/services/availability.ts
+++ src/services/availability.ts
@@ -3,6 +3,9 @@ import { Availability } from "@/models/availability";
 import { CreateAvailabilityDto } from "@/admin-api/availabilities/dtos/create-availability.dtos";
 import AvailabilityProductService from "./availability-product";
 import InternalServerError from "@/error/InternalServerError";
+import { GetAvailabilitiesDto } from "@/api/admin/availabilities/dtos/get-availabilities.dtos";
+import { FindOneOptions, MoreThan } from "typeorm";
+import { GetAvailabilitiesResponseDto } from "@/types/availability";
 
 class AvailabilityService extends TransactionBaseService {
   protected availabilityProductService: AvailabilityProductService;
@@ -10,6 +13,37 @@ class AvailabilityService extends TransactionBaseService {
   constructor(config) {
     super(config);
     this.availabilityProductService = config.availabilityProductService;
+  }
+
+  async get(
+    query: GetAvailabilitiesDto,
+  ): Promise<GetAvailabilitiesResponseDto> {
+    const availabilityRepo = this.activeManager_.getRepository(Availability);
+
+    const whereClose: FindOneOptions<Availability>["where"] = {};
+    const defaultQuery: GetAvailabilitiesDto = {
+      includeExpired: false,
+      limit: 10,
+      page: 0,
+    };
+
+    // expired availabilities are those whose dates have passed.
+    if (!query.includeExpired) {
+      whereClose.date = MoreThan(new Date());
+    }
+
+    const skipOffset = query.page
+      ? Math.max(query.page * query.limit, 0)
+      : defaultQuery.page;
+
+    const availabilities = await availabilityRepo.find({
+      where: whereClose,
+      skip: skipOffset,
+      take: query.limit || defaultQuery.limit,
+      order: { created_at: "DESC" },
+    });
+
+    return { availabilities };
   }
 
   async create(data: CreateAvailabilityDto) {
---
+++ src/types/availability.ts
@@ -0,0 +1,5 @@
+import { Availability } from "@/models/availability";
+
+export interface GetAvailabilitiesResponseDto {
+  availabilities: Availability[];
+}
