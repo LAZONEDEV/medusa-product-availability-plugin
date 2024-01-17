061c5245b26e70f579bd7e59b9ecdeb889ca2fbb
bocovo <bocovojuste@gmail.com>
2024-01-17 16:34:13 +0100

    refactor: improve entry exist validator

--- src/utils/validator/is-exist.ts
+++ src/utils/validator/is-exist.ts
@@ -5,6 +5,25 @@ import {
 } from "class-validator";
 import { dataSource } from "@medusajs/medusa/dist/loaders/database";
 
+const isExistsValidator = async (
+  value: string,
+  validationArguments: ValidationArguments,
+) => {
+  const [repository, pathToProperty] = validationArguments.constraints;
+
+  try {
+    const entity: unknown = await dataSource.getRepository(repository).findOne({
+      where: {
+        [pathToProperty]: value,
+      },
+    });
+    return Boolean(entity);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
 /**
  * Registers a decorator that checks if an entity exists in the database.
  */
@@ -21,31 +40,7 @@ export function IsExist(
       constraints: [entityName, entityPropertyName],
       options: validationOptions,
       validator: {
-        async validate(
-          value: string,
-          validationArguments: ValidationArguments,
-        ) {
-          const repository = validationArguments.constraints[0];
-          const pathToProperty = validationArguments.constraints[1];
-
-          try {
-            const entity: unknown = await dataSource
-              .getRepository(repository)
-              .findOne({
-                where: {
-                  [pathToProperty
-                    ? pathToProperty
-                    : validationArguments.property]: pathToProperty
-                    ? value?.[pathToProperty] || value
-                    : value,
-                },
-              });
-            return Boolean(entity);
-          } catch (error) {
-            console.error(error);
-            return false;
-          }
-        },
+        validate: isExistsValidator,
       },
     });
   };
