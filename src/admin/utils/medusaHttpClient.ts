b8a6487787af0256531998f87e8394db412f533d
bocovo <bocovojuste@gmail.com>
2024-01-19 15:09:51 +0100

    chore: create medusa http client

---
+++ src/admin/utils/medusaHttpClient.ts
@@ -0,0 +1,7 @@
+import HTTPClient from "./httpClient";
+
+const medusaHttpClient = new HTTPClient(process.env.MEDUSA_BACKEND_URL, {
+  credentials: "include",
+});
+
+export default medusaHttpClient;
