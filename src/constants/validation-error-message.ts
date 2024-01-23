dd5026b742575adf4de3e0974a94eaaf47f68f94
bocovo <bocovojuste@gmail.com>
2024-01-23 11:14:09 +0100

    chore: add uniqueness validators on create availability dto

---
+++ src/constants/validation-error-message.ts
@@ -0,0 +1,6 @@
+export const ValidationErrorMessage = {
+  availabilityAlreadyExist: "Availability already exist for this date",
+  noDuplicateProdAvailability:
+    "You can not create several availability for the same product",
+  productNotExists: "noDuplicateProdAvailability",
+};
