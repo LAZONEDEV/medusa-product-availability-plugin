432f1b5133cf18eb70165467cabc35f99237253a
bocovo <bocovojuste@gmail.com>
2024-01-23 14:08:46 +0100

    chore: create custom validator
    Create custom validator for better validation error format

--- src/constants/errors-message.ts
+++ src/constants/errors-message.ts
@@ -1,7 +1,9 @@
 export enum ErrorCode {
-  INTERNAL_SERVER_ERROR_MESSAGE = "INTERNAL_SERVER_ERROR_MESSAGE",
+  INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR",
+  VALIDATION_ERROR = "VALIDATION_ERROR",
 }
 
 export const ErrorMessages: Record<ErrorCode, string> = {
-  [ErrorCode.INTERNAL_SERVER_ERROR_MESSAGE]: "Internal server error",
+  [ErrorCode.INTERNAL_SERVER_ERROR]: "Internal server error",
+  [ErrorCode.VALIDATION_ERROR]: "Validation error",
 };
