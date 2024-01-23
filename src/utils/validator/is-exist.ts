b93807dcee7c19cba92181c0625f69522b664a3b
bocovo <bocovojuste@gmail.com>
2024-01-23 11:07:25 +0100

    chore: create validator to check that a record does not exits
    Create validator to check that in database that a record with some value does not exist

--- src/utils/validator/is-exist.ts
+++ src/utils/validator/is-exist.ts
@@ -27,20 +27,50 @@ const isExistsValidator = async (
 /**
  * Registers a decorator that checks if an entity exists in the database.
  */
-export function IsExist(
+export function IsExists(
   entityName: string,
   entityPropertyName: string,
-  validationOptions?: ValidationOptions,
+  options?: ValidationOptions,
 ) {
   return function (object: Object, propertyName: string) {
     registerDecorator({
-      name: "isExist",
+      options,
+      propertyName,
+      name: "isExists",
       target: object.constructor,
-      propertyName: propertyName,
       constraints: [entityName, entityPropertyName],
-      options: validationOptions,
       validator: {
         validate: isExistsValidator,
+      },
+    });
+  };
+}
+
+const doesNotExistsValidator = async (
+  value: string,
+  validationArguments: ValidationArguments,
+) => {
+  const response = await isExistsValidator(value, validationArguments);
+  return !response;
+};
+
+/**
+ * Registers a decorator that checks if an entity does not exists in the database.
+ */
+export function DoesNotExists(
+  entityName: string,
+  entityPropertyName: string,
+  options?: ValidationOptions,
+) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      options,
+      propertyName,
+      name: "doesNotExists",
+      target: object.constructor,
+      constraints: [entityName, entityPropertyName],
+      validator: {
+        validate: doesNotExistsValidator,
       },
     });
   };
