83f573695dcd470b189cfad3515f69f0021c24db
bocovo <bocovojuste@gmail.com>
2024-01-25 00:12:17 +0100

    chore(admin): create hook for availability creation

--- src/admin/hooks/availabilities.ts
+++ src/admin/hooks/availabilities.ts
@@ -1,10 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import { GetAvailabilityResponse } from "../types/api";
+import { FormikHelpers } from "formik";
+import { useToast } from "@medusajs/ui";
+import { useSearchParams, useNavigate } from "react-router-dom";
+
+import { CreateAvailabilityDto, GetAvailabilityResponse } from "../types/api";
 import medusaHttpClient from "../utils/medusaHttpClient";
 import medusaApiRoutes from "../constants/apiRoutes";
 import apiRequestKey from "../constants/apiRequestKey";
-import { useSearchParams } from "react-router-dom";
 import { AVAILABILITY_PER_PAGE } from "../configs/pagination";
+import BadRequestHttpClientError from "../errors/BadRequestHttpClientError";
+import { fieldErrorsToFormikErrors } from "../utils/fieldErrorToFormikError";
+import AvailabilityApiService from "../services/AvailabilityApiService";
+import adminRoutes from "../constants/adminRoutes";
 
 export const useGetAvailabilities = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -50,4 +57,48 @@ export const useGetAvailabilities = () => {
     refetch,
     pageSize: AVAILABILITY_PER_PAGE,
   };
+};
+
+export const useCreateAvailabilityMutation = () => {
+  const { toast } = useToast();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (
+    values: CreateAvailabilityDto,
+    formikHelpers: FormikHelpers<CreateAvailabilityDto>,
+  ) => {
+    try {
+      await AvailabilityApiService.create(values);
+
+      toast({
+        title: "Disponibilité créée",
+        description: "La disponibilité est créée avec succès",
+        variant: "success",
+      });
+
+      navigate(adminRoutes.availabilities);
+    } catch (error) {
+      toast({
+        title: "Échec de création de la disponibilité",
+        description: error.message,
+        variant: "error",
+      });
+      if (error instanceof BadRequestHttpClientError) {
+        const formikErrors = fieldErrorsToFormikErrors(error.payload);
+        formikHelpers.setErrors(formikErrors);
+      }
+
+      if (error.message === "VALIDATION_ERROR") {
+        toast({
+          title: "Informations incorrectes",
+          description:
+            "Veuillez corriger les erreurs du formulaire et réessayez",
+          variant: "error",
+        });
+        return;
+      }
+    }
+  };
+
+  return handleSubmit;
 };
