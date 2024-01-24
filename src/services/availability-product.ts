000391a28ddb4c51eedb2c766df20ef6e4ba0b3a
bocovo <bocovojuste@gmail.com>
2024-01-24 22:39:38 +0100

    chore(backend): use same transaction while creating availabilities

--- src/services/availability-product.ts
+++ src/services/availability-product.ts
@@ -1,43 +1,45 @@
 import { TransactionBaseService } from "@medusajs/medusa";
 import type { CreateAvailabilityProductDto } from "@/admin-api/availabilities/dtos/create-availability.dtos";
 import { AvailabilityProduct } from "@/models/product-availability";
-import { QueryFailedError } from "typeorm";
+import { EntityManager, QueryFailedError } from "typeorm";
 import { checkProductAvailabilityDuplicationError } from "@/utils/check-error";
 import BadRequestError from "@/error/BadRequestError";
 import { ValidationErrorMessage } from "@/constants/validation-error-message";
 
 class AvailabilityProductService extends TransactionBaseService {
-  async create(items: CreateAvailabilityProductDto[], availabilityId: string) {
+  async create(
+    items: CreateAvailabilityProductDto[],
+    availabilityId: string,
+    entityManager: EntityManager,
+  ): Promise<AvailabilityProduct[]> {
     const itemsWithAvailabilityId = items.map(({ productId, quantity }) => ({
       quantity,
       product: { id: productId },
       availability: { id: availabilityId },
     }));
 
-    await this.atomicPhase_(async (entityManager) => {
-      try {
-        const availabilityProdRepo =
-          entityManager.getRepository(AvailabilityProduct);
+    try {
+      const availabilityProdRepo =
+        entityManager.getRepository(AvailabilityProduct);
 
-        const availabilities = availabilityProdRepo.create(
-          itemsWithAvailabilityId,
+      const availabilities = availabilityProdRepo.create(
+        itemsWithAvailabilityId,
+      );
+
+      const result = await availabilityProdRepo.save(availabilities);
+      return result;
+    } catch (error) {
+      if (
+        error instanceof QueryFailedError &&
+        checkProductAvailabilityDuplicationError(error)
+      ) {
+        throw new BadRequestError(
+          ValidationErrorMessage.noDuplicateProdAvailability,
         );
-
-        const result = await availabilityProdRepo.save(availabilities);
-        return result;
-      } catch (error) {
-        if (
-          error instanceof QueryFailedError &&
-          checkProductAvailabilityDuplicationError(error)
-        ) {
-          throw new BadRequestError(
-            ValidationErrorMessage.noDuplicateProdAvailability,
-          );
-        }
-
-        throw error;
       }
-    });
+
+      throw error;
+    }
   }
 }
 
