b6b5fc044fe970a10131d0b63811b9a3295342cd
bocovo <bocovojuste@gmail.com>
2024-01-29 13:08:44 +0100

    feat(admin): create availability update page

---
+++ src/admin/components/availability/detail-page/AvailabilityDetailProductItem.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@medusajs/ui";
+import ProductAvailabilityItem from "../create-form/ProductAvailabilityItem";
+import { useField } from "formik";
+import FieldErrorMessage from "../../inputs/ErrorMessage";
+
+interface AvailabilityDetailProductItemProps {
+  isSubmitting: boolean;
+  title: string;
+  image: string;
+  onRemove: () => void;
+  isRemoving: boolean;
+}
+
+const AvailabilityDetailProductItem = ({
+  isSubmitting,
+  image,
+  title,
+  onRemove,
+  isRemoving,
+}: AvailabilityDetailProductItemProps) => {
+  const [{ value }, { initialValue }, { setValue }] = useField("quantity");
+
+  const valuedNotChanged = value === initialValue;
+  return (
+    <>
+      <ProductAvailabilityItem
+        title={title}
+        image={image}
+        quantity={value}
+        onQuantityChange={setValue}
+        onRemove={onRemove}
+        isRemoving={isRemoving}
+      >
+        <Button
+          disabled={valuedNotChanged}
+          isLoading={isSubmitting}
+          type="submit"
+        >
+          Mettre à jour
+        </Button>
+      </ProductAvailabilityItem>
+
+      <FieldErrorMessage name="quantity" />
+    </>
+  );
+};
+
+export default AvailabilityDetailProductItem;
