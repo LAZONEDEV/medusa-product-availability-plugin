bde1f001c57f19b5f62d072d0d3133553b462560
bocovo <bocovojuste@gmail.com>
2024-02-07 14:50:11 +0100

    fix(admin): add some margin on message text

--- src/admin/components/availability/EmptyList.tsx
+++ src/admin/components/availability/EmptyList.tsx
@@ -5,7 +5,9 @@ const EmptyList = () => {
     <div className="flex flex-col items-center justify-center py-8">
       <h1 className="text-3xl font-bold mb-2">Aucune disponibilité</h1>
 
-      <p className="text-gray-500">Commencez par créer vos disponibilités</p>
+      <p className="text-gray-500 mb-4">
+        Commencez par créer vos disponibilités
+      </p>
 
       <AddNewAvailabilityBtn>Créer une disponibilité</AddNewAvailabilityBtn>
     </div>
