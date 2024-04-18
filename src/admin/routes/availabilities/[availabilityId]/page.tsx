8f3816d5d9ddff172cef97ff5aa09b1c07881422
bocovo <bocovojuste@gmail.com>
2024-04-18 16:18:06 +0100

    feat(admin): allow availability status changing

--- src/admin/routes/availabilities/[availabilityId]/page.tsx
+++ src/admin/routes/availabilities/[availabilityId]/page.tsx
@@ -6,6 +6,7 @@ import AvailabilityDetailTitle from "../../../components/availability/detail-pag
 import AvailabilityDetailsProductsList from "../../../components/availability/detail-page/AvailabilityDetailsProductsList";
 import AddNewProductAvailabilitiesForm from "../../../components/availability/detail-page/AddNewProductAvailabilitiesForm";
 import DeleteAvailabilityPrompt from "../../../components/availability/detail-page/DeleteAvailabilityPrompt";
+import AvailabilityStatusUpdater from "../../../components/availability/detail-page/AvailabilityStatusUpdater";
 
 const Page = () => {
   const { data, error, isLoading, refetch, availabilityId } =
@@ -24,9 +25,16 @@ const Page = () => {
       <Toaster />
 
       <div className="mb-4">
-        <AvailabilityDetailTitle date={data.date} />
+        <AvailabilityDetailTitle date={data.date} status={data.status} />
 
-        <DeleteAvailabilityPrompt availabilityId={availabilityId} />
+        <div className="flex items-center justify-between">
+          <AvailabilityStatusUpdater
+            availabilityId={availabilityId}
+            status={data.status}
+          />
+
+          <DeleteAvailabilityPrompt availabilityId={availabilityId} />
+        </div>
       </div>
 
       <AvailabilityDetailsProductsList
